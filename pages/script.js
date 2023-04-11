20467eeddbc0cb830e9a003546c9fd0888968680
Beatriz Andrade <beandy.023@gmail.com>
2023-04-11 16:20:21 -0300

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,23 +1,13 @@
-window.onload = function () {
-  let ps = placeSearch({
-    key: "vNH4YmGf1pMUK1iVQcCCQFaUXQMMVb5u",
-    container: document.querySelector("#search-input"),
-    collection: ["adminArea", "address"],
-    useDeviceLocation: true,
-  });
-
   L.mapquest.key = "vNH4YmGf1pMUK1iVQcCCQFaUXQMMVb5u";
 
   let baseLayer = L.mapquest.tileLayer("map");
   let map = L.mapquest.map("map", {
-    center: [-23.5489, -46.6388],
+    center: [-23.515864, -46.625416],
     layers: baseLayer,
     zoom: 10,
   });
 
   L.mapquest.control().addTo(map);
-
-  let searchLayer = null;
 
   ps.on("change", (e) => {
     let zoom = 13;
@@ -26,7 +16,6 @@ window.onload = function () {
     }
 
     map.setView(e.result.latlng, zoom);
-    let search = L.mapquest.search();
 
     search.place(
       {
@@ -35,30 +24,6 @@ window.onload = function () {
         bbox: map.getBounds(),
         pageSize: 20,
       },
-      handleSearch
-    );
   });
 
-  function handleSearch(err, data) {
-    if (searchLayer) {
-      map.removeLayer(searchLayer);
-    }
-
-    searchLayer = L.mapquest.searchLayer({
-      searchResponse: data,
-    });
-    map.addLayer(searchLayer);
-  }
-
-  ps.on("clear", () => {
-    if (searchLayer) {
-      map.removeLayer(searchLayer);
-    }
-    searchLayer = null;
-    map.setView(new L.LatLng(-23.5489, -46.6388), 10);
-  });
-
-  ps.on("error", (e) => {
-    alert(e);
-  });
 };
