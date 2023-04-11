202f9697c7a3f150225cddc7519ac7246b667c21
Beatriz Andrade <beandy.023@gmail.com>
2023-04-11 19:27:09 -0300

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,8 +1,11 @@
-  L.mapquest.key = "vNH4YmGf1pMUK1iVQcCCQFaUXQMMVb5u";
+window.onload = function() {
+L.mapquest.key = "vNH4YmGf1pMUK1iVQcCCQFaUXQMMVb5u";
 
-  let baseLayer = L.mapquest.tileLayer("map");
   let map = L.mapquest.map("map", {
     center: [-23.515864, -46.625416],
-    layers: baseLayer,
-    zoom: 10,
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
   });
+
+  map.addControl(L.mapquest.control());
+      }
